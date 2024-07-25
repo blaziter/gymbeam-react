bb9cc391e6a8f26fd7d8fac9ea3c00a67f425e01
blaziter <petrblaztran@gmail.com>
2024-07-26 01:30:13 +0200

    feat: add options to tb function and add validation to function

--- src/lib/hooks/useTranslation.ts
+++ src/lib/hooks/useTranslation.ts
@@ -1,10 +1,16 @@
 import useTranslation from 'next-translate/useTranslation';
 
+import { isDefined, isEmptyString } from '@lib/validation';
+
 export const useNextTranslation = (scope?: string) => {
     const translation = useTranslation();
 
-    const tb = (key: string) => {
-        return translation.t(scope ? `${scope}.${key}` : key);
+    /* translation with base */
+    const tb = (key: string, options?: Record<string, string>) => {
+        return translation.t(
+            isDefined(!isEmptyString(scope)) ? `${scope}.${key}` : key,
+            options
+        );
     };
 
     return {
