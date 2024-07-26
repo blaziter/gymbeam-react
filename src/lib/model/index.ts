e78da2fb1c13e595424ad54d1ea4dcb0eff834f3
blaziter <petrblaztran@gmail.com>
2024-07-26 02:46:08 +0200

    feat(lib/model): add priority enum

--- src/lib/model/index.ts
+++ src/lib/model/index.ts
@@ -1,2 +1,3 @@
 export * from './list';
+export * from './priority';
 export * from './task';
---
+++ src/lib/model/priority.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+export enum Priority {
+    '1' = '#00FF00', // (Bright Green)
+    '2' = '#ADFF2F', // (Green Yellow)
+    '3' = '#9ACD32', // (Yellow Green)
+    '4' = '#FFFF00', // (Yellow)
+    '5' = '#FFD700', // (Gold)
+    '6' = '#FFA500', // (Orange)
+    '7' = '#FF8C00', // (Dark Orange)
+    '8' = '#FF4500', // (Orange Red)
+    '9' = '#FF0000', // (Red)
+    '10' = '#B22222', // (Firebrick)
+}
