a24a9dbb7fb034f88871feece766078cd81c9070
blaziter <petrblaztran@gmail.com>
2024-07-26 12:47:45 +0200

    feat(lib/hooks): define task endpoints

--- src/lib/hooks/useApi.ts
+++ src/lib/hooks/useApi.ts
@@ -2,7 +2,7 @@ import { UseQueryResult, useQuery } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
 import { axiosClient } from '@lib/client/axios';
-import { List } from '@lib/model';
+import { List, Task } from '@lib/model';
 
 export const useApi = <
     QueryFnData = unknown,
@@ -23,48 +23,142 @@ type FindAllListsResponseDto = List;
 
 type FindListResponseDto = List;
 
-type CreateListBodyDto = Omit<List, 'id' | 'createdAt'>;
+type CreateListBodyDto = {
+    name: string;
+};
 
-type UpdateListBodyDto = Omit<List, 'createdAt'>;
+type UpdateListBodyDto = CreateListBodyDto;
 
-type DeleteListParamsDto = Pick<List, 'id'>;
+type FindAllTasksParamsDto = {
+    listId: string;
+};
+
+type FindAllTasksResponseDto = Array<Task>;
+
+type FindTaskResponseDto = Omit<CreateTaskBodyDto, 'dueDate' | 'tags'> & {
+    tags: string[];
+    dueDate: Date;
+};
+
+type CreateTaskBodyDto = {
+    name?: string;
+    completed?: boolean;
+    priority?: string;
+    dueDate: Date;
+    tags?: string[];
+    etc?: string;
+};
+
+type UpdateTaskBodyDto = Omit<CreateTaskBodyDto, 'dueDate'> & {
+    dueDate?: Date;
+};
 
 export class todoListApi {
-    public static getAllLists = () => {
-        return request<FindAllListsResponseDto[]>({
-            url: '/todo-list',
-            method: 'GET',
-        });
+    public static getAllLists = {
+        key: 'get-lists',
+        call: () => {
+            return request<FindAllListsResponseDto[]>({
+                url: '/todo-list',
+                method: 'GET',
+            });
+        },
     };
 
-    public static getOneList = (id: string) => {
-        return request<FindListResponseDto>({
-            url: `/todo-list/${id}`,
-            method: 'GET',
-        });
+    public static getOneList = {
+        key: 'get-list',
+        call: ({ listId }: { listId: string }) => {
+            return request<FindListResponseDto>({
+                url: `/todo-list/${listId}`,
+                method: 'GET',
+            });
+        },
     };
 
-    public static createList = (data: CreateListBodyDto) => {
-        return request({
-            url: '/todo-list',
-            method: 'POST',
-            data,
-        });
+    public static createList = {
+        key: 'create-list',
+        call: (data: CreateListBodyDto) => {
+            return request({
+                url: '/todo-list',
+                method: 'POST',
+                data,
+            });
+        },
     };
 
-    public static updateList = (data: UpdateListBodyDto) => {
-        return request({
-            url: `/todo-list/${data.id}`,
-            method: 'PUT',
-            data,
-        });
+    public static updateList = {
+        key: 'update-list',
+        call: ({ listId }: { listId: string }, data: UpdateListBodyDto) => {
+            return request({
+                url: `/todo-list/${listId}`,
+                method: 'PUT',
+                data,
+            });
+        },
     };
 
-    public static deleteList = ({ id }: DeleteListParamsDto) => {
-        return request({
-            url: `/todo-list/${id}`,
-            method: 'DELETE',
-        });
+    public static deleteList = {
+        key: 'delete-list',
+        call: ({ listId }: { listId: string }) => {
+            return request({
+                url: `/todo-list/${listId}`,
+                method: 'DELETE',
+            });
+        },
+    };
+
+    public static getAllTasks = {
+        key: 'get-tasks',
+        call: ({ listId }: FindAllTasksParamsDto) => {
+            return request<FindAllTasksResponseDto>({
+                url: `/todo-list/${listId}/tasks`,
+                method: 'GET',
+            });
+        },
+    };
+
+    public static getTask = {
+        key: 'get-task',
+        call: ({ listId, taskId }: { listId: string; taskId: string }) => {
+            return request<FindTaskResponseDto>({
+                url: `/todo-list/${listId}/tasks/${taskId}`,
+                method: 'GET',
+            });
+        },
+    };
+
+    public static createTask = {
+        key: 'create-task',
+        call: ({ listId }: { listId: string }, data: CreateTaskBodyDto) => {
+            return request({
+                url: `/todo-list/${listId}/tasks`,
+                method: 'POST',
+                data,
+            });
+        },
+    };
+
+    public static updateTask = {
+        key: 'update-task',
+        call: (
+            { listId, taskId }: { listId: string; taskId: string },
+            data: UpdateTaskBodyDto
+        ) => {
+            return request({
+                url: `/todo-list/${listId}/tasks/${taskId}`,
+                method: 'PUT',
+                data,
+            });
+        },
+    };
+
+    public static deleteTask = {
+        key: 'delete-task',
+        call: ({ listId, taskId }: { listId: string; taskId: string }) => {
+            return request({
+                url: `/todo-list/${listId}/tasks/${taskId}`,
+                method: 'DELETE',
+            });
+        },
     };
 }
 
@@ -94,6 +188,9 @@ const request = async <T = any>({
     params,
     headers,
 }: RequestParams): Promise<AxiosResponse<T>> => {
+    if (url.includes('undefined'))
+        return Promise.resolve({} as AxiosResponse<T>);
+
     return axiosClient.request({
         url,
         method,
