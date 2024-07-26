b9cc8aab553f1b74c7f12176ac49aff01cb52910
blaziter <petrblaztran@gmail.com>
2024-07-26 13:23:39 +0200

    refactor: check empty strings in tags array

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,21 @@
 import { type ClassValue, clsx } from 'clsx';
+import { isEmpty } from 'lodash';
 import { twMerge } from 'tailwind-merge';
+
+import { isEmptyString } from './validation';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
 export const getTags = (tags: string): string[] => {
-    return tags.split(',').map((tag) => tag.trim());
+    const result = tags.split(',').map((tag) => tag.trim());
+
+    if (!isEmpty(result)) {
+        result.map(
+            (tag) => isEmptyString(tag) && result.splice(result.indexOf(tag), 1)
+        );
+    }
+
+    return result;
 };
